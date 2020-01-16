e5859d587df1efb494f111c7c1dfe863635603aa
matozog <226125@student.pwr.edu.pl>
2020-01-16 20:50:18 +0100

    move model code to function

--- src/main.py
+++ src/main.py
@@ -46,7 +46,7 @@ def count_plot_comparison(feature, data_df, tiff_data, dicom_data):
 
 def load_data(tif_data):
     RefDs = imread(tif_data['path'][0])
-    IMG_PX_SIZE = 256
+    IMG_PX_SIZE = 128
 
     ConstPixelDims = (len(tif_data), IMG_PX_SIZE, IMG_PX_SIZE)
 
@@ -75,8 +75,8 @@ def load_data(tif_data):
 
 
 def preprocessing_data(tifSet, labels):
-    filters_per_image = 20
-    input_shape = 256
+    filters_per_image = 100
+    input_shape = 128
     ConstPixelDims = (len(tifSet) * filters_per_image, input_shape, input_shape, 1)
     processedTIFSet = np.zeros(ConstPixelDims)
     processedLabels = np.zeros(len(labels) * filters_per_image, dtype=labels.dtype)
@@ -119,100 +119,77 @@ def show_images(images):
         plt.show()
 
 
-def main():
-    batch_size = 32
-    epochs = 2
-    input_shape = 256
-    n_classes = 2
-    data_dir = "../"
-    data_df = pd.read_csv(data_dir + "overview.csv")
-
-    tif_data = process_data(data_dir + 'tiff_images/*.tif')
-
-    # count_plot_comparison('Age', data_df, tiff_data, dicom_data)
-
-    print("Loading tiff data ...")
-    tifSet, labels = load_data(tif_data)
-
-    print("Preprocessing data ...")
-    processedTIFSet, processedLabels = preprocessing_data(tifSet, labels)
-
-    X_train, X_test, y_train, y_test = train_test_split(processedTIFSet, processedLabels, test_size=0.143, shuffle=True,
-                                                        random_state=50)
-
-    X_train = X_train.reshape(-1, input_shape, input_shape, 1)
-    X_test = X_test.reshape(-1, input_shape, input_shape, 1)
-
-    X_train = X_train.astype('float32')
-    X_test = X_test.astype('float32')
-    X_train = X_train / float(pow(2, 16))
-    X_test = X_test / float(pow(2, 16))
-
-    y_train_one_hot = to_categorical(y_train)
-    y_test_one_hot = to_categorical(y_test)
-
-    X_train, X_valid, y_train, y_valid = train_test_split(X_train, y_train_one_hot, test_size=0.1, random_state=13,
-                                                          shuffle=True)
-    # loading model
-    # fashion_model = load_model("model_dropout.h5py")
-
-    print("Building model ...")
-    first_model = Sequential()
+def first_model(input_shape, n_classes):
+    model = Sequential()
     # first article
-    # first_model.add(
-    #     Conv2D(8, kernel_size=(5, 5), strides=(1, 1), activation='relu', input_shape=(input_shape, input_shape, 1)))
-    # first_model.add(MaxPooling2D((2, 2), strides=(2, 2)))
-    # first_model.add(Conv2D(16, kernel_size=(5, 5), strides=(1, 1), activation='relu'))
-    # first_model.add(MaxPooling2D((2, 2), strides=(2, 2)))
-    # first_model.add(Flatten())
-    # first_model.add(Dense(150, activation='relu'))
-    # first_model.add(Dense(100, activation='relu'))
-    # first_model.add(Dense(50, activation='relu'))
-    # first_model.add(Dense(n_classes, activation='softmax'))
+    model.add(
+        Conv2D(8, kernel_size=(5, 5), strides=(1, 1), activation='relu', input_shape=(input_shape, input_shape, 1)))
+    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+    model.add(Conv2D(16, kernel_size=(5, 5), strides=(1, 1), activation='relu'))
+    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+    model.add(Flatten())
+    model.add(Dense(150, activation='relu'))
+    model.add(Dense(100, activation='relu'))
+    model.add(Dense(50, activation='relu'))
+    model.add(Dense(n_classes, activation='softmax'))
 
+    # first article
+    sgd = keras.optimizers.SGD(learning_rate=0.0005, momentum=0.95)
+    # adam = keras.optimizers.Adam(learning_rate=0.0005)
+
+    model.compile(loss=keras.losses.categorical_crossentropy, optimizer=sgd,
+                  metrics=['accuracy'])
+    model.summary()
+
+    return model
+
+
+def second_model(input_shape, n_classes):
+    model = Sequential()
     # second article
-    first_model.add(
+    model.add(
         Conv2D(20, kernel_size=(7, 7), strides=(1, 1), activation='relu', input_shape=(input_shape, input_shape, 1),
                bias_initializer=keras.initializers.Constant(value=0.0)))
-    first_model.add(ReLU(max_value=None, negative_slope=0.0, threshold=0.0))
-    first_model.add(MaxPooling2D((2, 2), strides=(2, 2)))
-    first_model.add(Conv2D(50, kernel_size=(7, 7), strides=(1, 1), activation='relu',
-                           bias_initializer=keras.initializers.Constant(value=0.1)))
-    first_model.add(ReLU(max_value=None, negative_slope=0.0, threshold=0.0))
-    first_model.add(MaxPooling2D((2, 2), strides=(2, 2)))
-    first_model.add(Flatten())
-    first_model.add(Dense(500, activation='relu', bias_initializer=keras.initializers.Constant(value=0.1)))
-    first_model.add(ReLU(max_value=None, negative_slope=0.0, threshold=0.0))
-    first_model.add(Dropout(0.5))
-    first_model.add(Dense(2, activation='softmax', bias_initializer=keras.initializers.Constant(value=0.0)))
-
-    # third model
-    # first_model.add(Conv2D(32, kernel_size=(4, 4), activation='relu', input_shape=(input_shape, input_shape, 1)))
-    # first_model.add(MaxPooling2D((2, 2), strides=(2, 2)))
-    # first_model.add(Flatten())
-    # first_model.add(Dense(n_classes, activation='softmax'))
-
-    # first article
-    # sgd = keras.optimizers.SGD(learning_rate=0.0005, momentum=0.95)
-    # adam = keras.optimizers.Adam(learning_rate=0.0005)
+    model.add(ReLU(max_value=None, negative_slope=0.0, threshold=0.0))
+    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+    model.add(Conv2D(50, kernel_size=(7, 7), strides=(1, 1), activation='relu',
+                     bias_initializer=keras.initializers.Constant(value=0.1)))
+    model.add(ReLU(max_value=None, negative_slope=0.0, threshold=0.0))
+    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+    model.add(Flatten())
+    model.add(Dense(500, activation='relu', bias_initializer=keras.initializers.Constant(value=0.1)))
+    model.add(ReLU(max_value=None, negative_slope=0.0, threshold=0.0))
+    model.add(Dropout(0.5))
+    model.add(Dense(n_classes, activation='softmax', bias_initializer=keras.initializers.Constant(value=0.0)))
 
     # second article
     sgd = keras.optimizers.SGD(learning_rate=0.0005, decay=5.5)
 
+    model.compile(loss=keras.losses.categorical_crossentropy, optimizer=sgd,
+                  metrics=['accuracy'])
+    model.summary()
+
+    return model
+
+
+def third_model(input_shape, n_classes):
+    model = Sequential()
     # third model
-    # adam = keras.optimizers.Adam()
+    model.add(Conv2D(32, kernel_size=(4, 4), activation='relu', input_shape=(input_shape, input_shape, 1)))
+    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+    model.add(Flatten())
+    model.add(Dense(n_classes, activation='softmax'))
+    # third model
+    adam = keras.optimizers.Adam()
 
-    first_model.compile(loss=keras.losses.categorical_crossentropy, optimizer=sgd,
-                        metrics=['accuracy'])
+    model.compile(loss=keras.losses.categorical_crossentropy, optimizer=adam,
+                  metrics=['accuracy'])
+    model.summary()
 
-    # first_model.save("first_model.h5py")
-    first_model.summary()
+    return model
 
-    first_train = first_model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs, verbose=1,
-                                  validation_data=(X_valid, y_valid))
 
-    test_eval = first_model.evaluate(X_test, y_test_one_hot, verbose=0)
-
+def evaluate_results(first_train, model, test_eval, X_test, y_test, input_shape, n_classes):
     print('Test loss:', test_eval[0])
     print('Test accuracy:', test_eval[1])
 
@@ -232,7 +209,7 @@ def main():
     plt.legend()
     plt.show()
 
-    predicted_classes = first_model.predict(X_test)
+    predicted_classes = model.predict(X_test)
     predicted_classes = np.argmax(np.round(predicted_classes), axis=1)
 
     correct = np.where(predicted_classes == y_test)[0]
@@ -258,6 +235,103 @@ def main():
 
     confusion_matrix_result = confusion_matrix(y_test, predicted_classes)
     print(confusion_matrix_result)
+
+
+def main():
+    batch_size = 64
+    epochs = 2
+    input_shape = 128
+    n_classes = 2
+    data_dir = "../"
+    data_df = pd.read_csv(data_dir + "overview.csv")
+
+    tif_data = process_data(data_dir + 'tiff_images/*.tif')
+
+    # count_plot_comparison('Age', data_df, tiff_data, dicom_data)
+
+    print("Loading tif data ...")
+    tifSet, labels = load_data(tif_data)
+
+    print("Preprocessing data ...")
+    processedTIFSet, processedLabels = preprocessing_data(tifSet, labels)
+
+    X_train, X_test, y_train, y_test = train_test_split(processedTIFSet, processedLabels, test_size=0.143, shuffle=True,
+                                                        random_state=50)
+
+    X_train = X_train.reshape(-1, input_shape, input_shape, 1)
+    X_test = X_test.reshape(-1, input_shape, input_shape, 1)
+
+    X_train = X_train.astype('float32')
+    X_test = X_test.astype('float32')
+    X_train = X_train / float(pow(2, 16))
+    X_test = X_test / float(pow(2, 16))
+
+    y_train_one_hot = to_categorical(y_train)
+    y_test_one_hot = to_categorical(y_test)
+
+    X_train, X_valid, y_train, y_valid = train_test_split(X_train, y_train_one_hot, test_size=0.1, random_state=13,
+                                                          shuffle=True)
+    # loading model
+    # fashion_model = load_model("model_dropout.h5py")
+
+    train_True = 0
+    train_False = 0
+    for category in y_train:
+        if category[0] == 0. and category[1] == 1.:
+            train_True += 1
+        else:
+            train_False += 1
+
+    valid_True = 0
+    valid_False = 0
+    for category in y_valid:
+        if category[0] == 0. and category[1] == 1.:
+            valid_True += 1
+        else:
+            valid_False += 1
+
+    test_True = 0
+    test_False = 0
+    for category in y_test_one_hot:
+        if category[0] == 0. and category[1] == 1.:
+            test_True += 1
+        else:
+            test_False += 1
+
+    print("Training set has: " + str(train_True) + " samples with contrast and " + str(
+        train_False) + " with no contrast. " + str(len(y_train)))
+    print("Validating set has: " + str(valid_True) + " samples with contrast and " + str(
+        valid_False) + " with no contrast. " + str(len(y_valid)))
+    print("Testing set has: " + str(test_True) + " samples with contrast and " + str(
+        test_False) + " with no contrast. " + str(len(y_test_one_hot)))
+
+    print("Building model ...")
+
+    model_1 = first_model(input_shape, n_classes)
+
+    model_2 = second_model(input_shape, n_classes)
+
+    model_3 = third_model(input_shape, n_classes)
+
+    # first_model.save("first_model.h5py")
+
+    first_train = model_1.fit(X_train, y_train, batch_size=batch_size, epochs=epochs, verbose=1,
+                              validation_data=(X_valid, y_valid))
+    test_eval_1 = model_1.evaluate(X_test, y_test_one_hot, verbose=0)
+    print("\nFIRST MODEL RESULTS !!!!")
+    evaluate_results(first_train, model_1, test_eval_1, X_test, y_test, input_shape, n_classes)
+
+    second_train = model_2.fit(X_train, y_train, batch_size=batch_size, epochs=epochs, verbose=1,
+                               validation_data=(X_valid, y_valid))
+    test_eval_2 = model_2.evaluate(X_test, y_test_one_hot, verbose=0)
+    print("\nSECOND MODEL RESULTS !!!!")
+    evaluate_results(second_train, model_2, test_eval_2, X_test, y_test, input_shape, n_classes)
+
+    third_train = model_3.fit(X_train, y_train, batch_size=batch_size, epochs=epochs, verbose=1,
+                              validation_data=(X_valid, y_valid))
+    test_eval_3 = model_3.evaluate(X_test, y_test_one_hot, verbose=0)
+    print("\nTHIRD MODEL RESULTS !!!!")
+    evaluate_results(third_train, model_3, test_eval_3, X_test, y_test, input_shape, n_classes)
 
 
 if __name__ == "__main__":
